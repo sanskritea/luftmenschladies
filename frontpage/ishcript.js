4a0271acf6d7c84db813eacae2c1a47831d697ac
Sanskriti Chitransh <65899165+sanskritea@users.noreply.github.com>
2020-07-06 22:28:20 +0530

    Update ishcript.js

--- frontpage/ishcript.js
+++ frontpage/ishcript.js
@@ -4,6 +4,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById("header").style.fontSize = "30px";
   } else {
-    document.getElementById("header").style.fontSize = "60px";
+    document.getElementById("header").style.fontSize = "80px";
   }
 }
