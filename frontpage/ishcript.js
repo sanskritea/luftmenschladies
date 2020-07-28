8c7ea37103c2dd7f5317a70770d22af6a4574d19
Sanskriti Chitransh <65899165+sanskritea@users.noreply.github.com>
2020-07-28 15:58:16 +0530

    Update ishcript.js

--- frontpage/ishcript.js
+++ frontpage/ishcript.js
@@ -1,5 +1,4 @@
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     document.getElementById("header").style.fontSize = "30px";
@@ -10,15 +9,8 @@ function scrollFunction() {
 
 
 document.getElementById("navMenu").innerHTML =
-  '<p>'+  
-    '<a href="https://sanskritea.github.io/luftmenschladies/index.html">Frontpage</a><br>'+
-	  '<a href="https://sanskritea.github.io/luftmenschladies/about/about.html">About Us</a><br>'+
-	'</p>'+
-  '<p>+
-	  '<a href="https://sanskritea.github.io/luftmenschladies/posts/1/1.html">I</a><br>'+
-		'<a href="https://sanskritea.github.io/luftmenschladies/posts/2/2.html">II</a><br>'+
-		'<a href="https://sanskritea.github.io/luftmenschladies/posts/3/3.html">III</a><br>'+
-     '<a href="https://sanskritea.github.io/luftmenschladies/posts/4/4.html">IV</a><br>'+
-      '<a href="https://sanskritea.github.io/luftmenschladies/posts/5/5.html">V</a><br>'+
-      '<a href="https://sanskritea.github.io/luftmenschladies/posts/6/6.html">VI</a><br>'+
-  '</p>';
+  '<ul>'+
+  '<li><a href="index.html">Home</a></li>'+
+  '<li><a href="services.html">Services</a></li>'+
+  '<li><a href="about.html">About</a></li>'+
+ '</ul>';
